7153d0330380ba047dcddf66f66288d517376cbe
Armando Lüscher <armando@noplanman.ch>
2015-07-27 01:49:40 +0200

    Set enabled Soil fixes.

--- themes/btcsumo/lib/config.php
+++ themes/btcsumo/lib/config.php
@@ -5,13 +5,16 @@ namespace Roots\Sage\Config;
 use Roots\Sage\ConditionalTagCheck;
 
 /**
- * Enable theme features
+ * Enable theme features.
  */
-add_theme_support('soil-clean-up');         // Enable clean up from Soil
-add_theme_support('soil-nav-walker');       // Enable cleaner nav walker from Soil
-add_theme_support('soil-relative-urls');    // Enable relative URLs from Soil
-add_theme_support('soil-nice-search');      // Enable nice search from Soil
-add_theme_support('soil-jquery-cdn');       // Enable to load jQuery from the Google CDN
+add_theme_support( 'soil-clean-up' );      // Enable clean up from Soil.
+add_theme_support( 'soil-js-to-footer' );  // Move all JS to footer.
+add_theme_support( 'soil-disable-asset-versioning' ); // Disable version queries of assets.
+
+//add_theme_support( 'soil-nav-walker' );    // Enable cleaner nav walker from Soil.
+//add_theme_support( 'soil-relative-urls' ); // Enable relative URLs from Soil.
+//add_theme_support( 'soil-nice-search' );   // Enable nice search from Soil.
+//add_theme_support( 'soil-jquery-cdn' );    // Enable to load jQuery from the Google CDN.
 
 /**
  * Configuration values
