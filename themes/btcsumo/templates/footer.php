49a0b37ba17cc7f890efa4a33892b4e315d54898
Armando Lüscher <armando@noplanman.ch>
2015-08-03 01:14:58 +0200

    Add powered by logos to footer.

--- themes/btcsumo/templates/footer.php
+++ themes/btcsumo/templates/footer.php
@@ -1,11 +1,20 @@
+<?php use Roots\Sage\Assets; ?>
 <footer class="footer content-info" role="contentinfo">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-md-6 col-lg-4">
-        <?php dynamic_sidebar('sidebar-footer'); ?>
-      </div>
-      <div class="col-xs-12 col-md-6 col-lg-4">
-        Powered by: WordPress, Bootstrap, Sage, etc.
+      <div class="col-xs-12">
+        <ul class="powered-by">
+          <li><?= __( 'Powered by:', 'btcsumo' ); ?>
+          <li><a href="http://www.wordpress.org/" target="_blank" rel="nofollow">
+            <img src="<?= Assets\asset_path( 'images/logos/wordpress.png' );?>" alt="WordPress">
+          </a>
+          <li><a href="http://www.roots.io/sage/" target="_blank" rel="nofollow">
+            <img src="<?= Assets\asset_path( 'images/logos/sage.png' );?>" alt="Sage">
+          </a>
+          <li><a href="http://getbootstrap.org/" target="_blank" rel="nofollow">
+            <img src="<?= Assets\asset_path( 'images/logos/bootstrap.png' );?>" alt="Bootstrap">
+          </a>
+        </ul>
       </div>
     </div>
   </div>
