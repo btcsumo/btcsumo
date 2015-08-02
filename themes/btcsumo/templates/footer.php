71374737cfbdbb03fef5f06c1246e144d4818b94
Armando Lüscher <armando@noplanman.ch>
2015-08-03 01:19:54 +0200

    Fix Bootstrap url and little typos.

--- themes/btcsumo/templates/footer.php
+++ themes/btcsumo/templates/footer.php
@@ -6,13 +6,13 @@
         <ul class="powered-by">
           <li><?= __( 'Powered by:', 'btcsumo' ); ?>
           <li><a href="http://www.wordpress.org/" target="_blank" rel="nofollow">
-            <img src="<?= Assets\asset_path( 'images/logos/wordpress.png' );?>" alt="WordPress">
+            <img src="<?= Assets\asset_path( 'images/logos/wordpress.png' ); ?>" alt="WordPress">
           </a>
           <li><a href="http://www.roots.io/sage/" target="_blank" rel="nofollow">
-            <img src="<?= Assets\asset_path( 'images/logos/sage.png' );?>" alt="Sage">
+            <img src="<?= Assets\asset_path( 'images/logos/sage.png' ); ?>" alt="Sage">
           </a>
-          <li><a href="http://getbootstrap.org/" target="_blank" rel="nofollow">
-            <img src="<?= Assets\asset_path( 'images/logos/bootstrap.png' );?>" alt="Bootstrap">
+          <li><a href="http://getbootstrap.com/" target="_blank" rel="nofollow">
+            <img src="<?= Assets\asset_path( 'images/logos/bootstrap.png' ); ?>" alt="Bootstrap">
           </a>
         </ul>
       </div>
