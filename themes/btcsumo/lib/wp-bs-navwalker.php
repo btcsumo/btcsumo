3d77c408804cb0a9c65b9edc90a18f4fe91fb464
Armando Lüscher <armando@noplanman.ch>
2015-08-06 21:02:54 +0200

    Optimise a bit more and refactor by extracting a few methods.

--- themes/btcsumo/lib/wp-bs-navwalker.php
+++ themes/btcsumo/lib/wp-bs-navwalker.php
@@ -17,12 +17,62 @@ class WP_Bootstrap_Nav_Walker extends Walker_Nav_Menu {
    * @since 3.0.0
    *
    * @param string $output Passed by reference. Used to append additional content.
-   * @param int    $depth  Depth of page. Used for padding.
+   * @param int    $depth  Depth of page.
    * @param array  $args
    */
   public function start_lvl( &$output, $depth = 0, $args = [] ) {
-    $indent = str_repeat( "\t", $depth );
-    $output .= "\n$indent<ul role=\"menu\" class=\" dropdown-menu\">\n";
+    $output .= '<ul role="menu" class="dropdown-menu">';
+  }
+
+  /**
+   * Get the attributes for the menu item.
+   * @param  object $item   Menu item data object.
+   * @param  int    $depth  Depth of menu item.
+   * @param  array  $args
+   * @return string         Attributes for the menu item.
+   */
+  private function _get_item_attributes( $item, $depth, $args ) {
+    $atts = [];
+    $atts['title']  = ( empty( $item->title ) )  ? '' : $item->title;
+    $atts['target'] = ( empty( $item->target ) ) ? '' : $item->target;
+    $atts['rel']    = ( empty( $item->xfn ) )    ? '' : $item->xfn;
+
+    // If item has_children add atts to a.
+    if ( $args->has_children && 0 === $depth ) {
+      $atts['href']          = '#';
+      $atts['data-toggle']   = 'dropdown';
+      $atts['class']         = 'dropdown-toggle';
+      $atts['aria-haspopup'] = 'true';
+    } else {
+      $atts['href'] = ( empty( $item->url ) ) ? '' : $item->url;
+    }
+
+    $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+    $attributes = '';
+    foreach ( $atts as $attr => $value ) {
+      if ( ! empty( $value ) ) {
+        $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+        $attributes .= sprintf( ' %s="%s"', $attr, $value );
+      }
+    }
+    return $attributes;
+  }
+
+  /**
+   * Get the class attribute with all the item's class names.
+   * @param  object $item Menu item data object.
+   * @param  array  $args
+   * @return string       Class attribute for menu item.
+   */
+  private function _get_item_class_names( $item, $args ) {
+    $classes   = ( empty( $item->classes ) ) ? [] : (array) $item->classes;
+    $classes[] = 'menu-item-' . $item->ID;
+    $classes[] = ( $args->has_children ) ? 'dropdown' : '';
+    $classes[] = ( in_array( 'current-menu-item', $classes ) ) ? 'active' : '';
+
+    $classes = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+    return ( $classes ) ? ' class="' . esc_attr( $classes ) . '"' : '';
   }
 
   /**
@@ -31,90 +81,41 @@ class WP_Bootstrap_Nav_Walker extends Walker_Nav_Menu {
    *
    * @param string $output Passed by reference. Used to append additional content.
    * @param object $item   Menu item data object.
-   * @param int    $depth  Depth of menu item. Used for padding.
+   * @param int    $depth  Depth of menu item.
    * @param array  $args
    * @param int    $id     Menu item ID.
    */
   public function start_el( &$output, $item, $depth = 0, $args = [], $id = 0 ) {
-    $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-
-    /**
-     * Dividers, Headers or Disabled
-     * =============================
-     * Determine whether the item is a Divider, Header, Disabled or regular
-     * menu item. To prevent errors we use the strcasecmp() function to so a
-     * comparison that is not case sensitive. The strcasecmp() function returns
-     * a 0 if the strings are equal.
-     */
-    if ( strcasecmp( $item->attr_title, 'divider' ) == 0 && $depth === 1 ) {
-      $output .= $indent . '<li role="presentation" class="divider">';
-    } else if ( strcasecmp( $item->title, 'divider') == 0 && $depth === 1 ) {
-      $output .= $indent . '<li role="presentation" class="divider">';
-    } else if ( strcasecmp( $item->attr_title, 'dropdown-header') == 0 && $depth === 1 ) {
-      $output .= $indent . '<li role="presentation" class="dropdown-header">' . esc_attr( $item->title );
-    } else if ( strcasecmp($item->attr_title, 'disabled' ) == 0 ) {
-      $output .= $indent . '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
+    // Determine whether the item is a Divider, Header, Disabled or regular menu item.
+    if ( 1 === $depth && 0 === strcasecmp( $item->attr_title, 'dropdown-header' ) ) {
+      $output .= '<li role="presentation" class="dropdown-header">' . esc_attr( $item->title );
+    } elseif ( 1 === $depth && ( 0 == strcasecmp( $item->attr_title, 'divider' ) || 0 == strcasecmp( $item->title, 'divider') ) ) {
+      $output .= '<li role="presentation" class="divider">';
+    } elseif ( 0 === strcasecmp( $item->attr_title, 'disabled' ) ) {
+      $output .= '<li role="presentation" class="disabled"><a href="#">' . esc_attr( $item->title ) . '</a>';
     } else {
-
-      $classes = empty( $item->classes ) ? [] : (array) $item->classes;
-      $classes[] = 'menu-item-' . $item->ID;
-      $classes[] = ( $args->has_children ) ? 'dropdown' : '';
-      $classes[] = ( in_array( 'current-menu-item', $classes ) ) ? 'active' : '';
-
-      $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-      $class_names = ( $class_names ) ? ' class="' . esc_attr( $class_names ) . '"' : '';
-
-      $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+      // Set up the id attribute.
+      $id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args );
       $id = ( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
 
-      $output .= $indent . '<li' . $id . $class_names . '>';
+      // Get the class names attribute.
+      $class_names = $this->_get_item_class_names( $item, $args );
 
-      $atts = [];
-      $atts['title']  = ( ! empty( $item->title ) )  ? $item->title  : '';
-      $atts['target'] = ( ! empty( $item->target ) ) ? $item->target : '';
-      $atts['rel']    = ( ! empty( $item->xfn ) )    ? $item->xfn    : '';
+      $item_output = '<a' . $this->_get_item_attributes( $item, $depth, $args ) . '>';
 
-      // If item has_children add atts to a.
-      if ( $args->has_children && 0 === $depth ) {
-        $atts['href']          = '#';
-        $atts['data-toggle']   = 'dropdown';
-        $atts['class']         = 'dropdown-toggle';
-        $atts['aria-haspopup'] = 'true';
-      } else {
-        $atts['href'] = ( ! empty( $item->url ) ) ? $item->url : '';
-      }
-
-
-      $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
-
-      $attributes = '';
-      foreach ( $atts as $attr => $value ) {
-        if ( ! empty( $value ) ) {
-          $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
-          $attributes .= sprintf( ' %s="%s"', $attr, $value );
-        }
-      }
-
-      $item_output = $args->before;
-
-      /*
-       * Font Awesome
-       * ============
-       * Since the the menu item is NOT a Divider or Header we check to see
-       * if there is a value in the attr_title property. If the attr_title
-       * property is NOT null we apply it as the class name for the font awesome icon.
-       */
-      $item_output .= '<a' . $attributes . '>';
+      // Since the the menu item is NOT a Divider or Header, use the attr_title property for an icon.
       if ( ! empty( $item->attr_title ) ) {
         $item_output .= '<i class="fa ' . esc_attr( $item->attr_title ) . '"></i>&nbsp;';
       }
 
+      // Put the link together.
       $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
       $item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="caret"></span></a>' : '</a>';
 
-      $item_output .= $args->after;
+      // Put the item together.
+      $item_output = $args->before . $item_output . $args->after;
 
-      $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+      $output .= '<li' . $id . $class_names . '>' . apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
     }
   }
 
@@ -139,30 +140,28 @@ class WP_Bootstrap_Nav_Walker extends Walker_Nav_Menu {
    * @return null                      Null on failure with no changes to parameters.
    */
   public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
-        if ( ! $element ) {
-          return;
-        }
-
-        $id_field = $this->db_fields['id'];
-
-        // Display this element.
-        if ( is_object( $args[0] ) ) {
-          $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
-        }
-
-        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+    if ( ! $element ) {
+      return;
     }
 
+    // Display this element.
+    if ( is_object( $args[0] ) ) {
+      $id_field = $this->db_fields['id'];
+      $args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
+    }
+
+    parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+  }
+
   /**
-   * Menu Fallback
-   * =============
+   * Menu fallback callback.
+   *
    * If this function is assigned to the wp_nav_menu's fallback_cb variable
    * and a manu has not been assigned to the theme location in the WordPress
    * menu manager the function with display nothing to a non-logged in user,
    * and will add a link to the WordPress menu manager if logged in as an admin.
    *
    * @param array $args Passed from the wp_nav_menu function.
-   *
    */
   public static function fallback( $args ) {
     if ( current_user_can( 'manage_options' ) ) {
@@ -174,7 +173,7 @@ class WP_Bootstrap_Nav_Walker extends Walker_Nav_Menu {
         ( $menu_id )    ? ' id="'    . $menu_id    . '"' : '',
         ( $menu_class ) ? ' class="' . $menu_class . '"' : '',
         esc_url( admin_url( 'nav-menus.php' ) ),
-        __( 'Add a menu', 'btcsumo' )
+        __( 'Add a menu' )
       );
 
       if ( $container ) {
