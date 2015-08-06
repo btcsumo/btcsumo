1d293a991da6ad6b1a2468cc05a947a7bf6ba1fe
Armando Lüscher <armando@noplanman.ch>
2015-08-06 23:46:43 +0200

    templates/feed-box.php

--- themes/btcsumo/templates/feed-box.php
+++ themes/btcsumo/templates/feed-box.php
@@ -19,10 +19,10 @@ if ( $feed_items = Feeds\fetch_feed_items( get_the_ID(), 0, 5, $has_more ) ) : ?
       <div class="panel-heading">
         <h3 class="panel-title">
           <?php if ( ! empty( $twitter_url ) ) : ?>
-          <a href="<?= $twitter_url; ?>" target="_blank" class="feed-twitter fa fa-twitter" title="<?= __( 'Twitter', 'btcsumo' ); ?>"></a>
+          <a href="<?= $twitter_url; ?>" target="_blank" class="feed-twitter fa fa-twitter" title="<?php _e( 'Twitter', 'btcsumo' ); ?>"></a>
           <?php endif; ?>
           <a href="<?= $site_url; ?>" target="_blank"><?php the_title(); ?></a>
-          <span class="feed-refresh fa fa-refresh" title="<?= __( 'Refresh', 'btcsumo' ); ?>"></span>
+          <span class="feed-refresh fa fa-refresh" title="<?php _e( 'Refresh', 'btcsumo' ); ?>"></span>
         </h3>
       </div>
       <ul class="feed-list list-group" data-feed-id="<?= the_ID(); ?>" data-feed-start="0" data-feed-count="5">
@@ -37,10 +37,10 @@ if ( $feed_items = Feeds\fetch_feed_items( get_the_ID(), 0, 5, $has_more ) ) : ?
         <i class="load-spinner fa fa-refresh fa-spin"></i>
         <div class="row">
           <a class="btn btn-link load-newer col-xs-6 disabled">
-            <?= __( 'Newer', 'btcsumo' ); ?>
+            <?php _e( 'Newer', 'btcsumo' ); ?>
           </a>
           <a class="btn btn-link load-older col-xs-6<?= ( ! $has_more ) ? ' disabled' : ''; ?>">
-            <?= __( 'Older', 'btcsumo' ); ?>
+            <?php _e( 'Older', 'btcsumo' ); ?>
           </a>
         </div>
       </div>
