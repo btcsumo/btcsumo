5312b5ee61b853cdfe05c8adee8eadaf2b76289b
Armando Lüscher <armando@noplanman.ch>
2015-08-07 00:38:16 +0200

    lib/utils.php

--- themes/btcsumo/lib/utils.php
+++ themes/btcsumo/lib/utils.php
@@ -7,10 +7,10 @@ namespace Roots\Sage\Utils;
  */
 function get_search_form() {
   $form = '';
-  locate_template('/templates/searchform.php', true, false);
+  locate_template( '/templates/searchform.php', true, false );
   return $form;
 }
-add_filter('get_search_form', __NAMESPACE__ . '\\get_search_form');
+add_filter( 'get_search_form', __NAMESPACE__ . '\\get_search_form' );
 
 /**
  * Check if a value is empty and return the value if not.
