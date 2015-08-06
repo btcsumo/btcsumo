b146292a17a29dc9a61b1ba006f55ad1bc7e33b4
Armando Lüscher <armando@noplanman.ch>
2015-08-06 23:47:43 +0200

    templates/footer.php

--- themes/btcsumo/templates/footer.php
+++ themes/btcsumo/templates/footer.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-xs-12">
         <ul class="powered-by">
-          <li><?= __( 'Powered by:', 'btcsumo' ); ?>
+          <li><?php _e( 'Powered by:', 'btcsumo' ); ?>
           <li><a href="http://www.wordpress.org/" target="_blank" rel="nofollow">
             <img src="<?= Assets\asset_path( 'images/logos/wordpress.png' ); ?>" alt="WordPress">
           </a>
